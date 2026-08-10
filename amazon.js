606574a69c39ff2888b2dc3625b3b24c1130e273
dipak0291 <118797097+dipak0291@users.noreply.github.com>
2026-08-10 18:18:28 +0530

    Update amazon.js

--- amazon.js
+++ amazon.js
@@ -488,8 +488,11 @@ function buildListingPayload(product, images = []) {
         model_number: [{ value: product.sku, language_tag: lang, marketplace_id: mid }],
         part_number: [{ value: product.sku, language_tag: lang, marketplace_id: mid }],
         product_expiration_type: [  { value: "None", language_tag: lang,marketplace_id: MARKETPLACE_ID }],
-       external_product_information: [  {    value: product.gtin || "N/A",    type: product.gtinType || "EAN",
-    language_tag: lang,marketplace_id: MARKETPLACE_ID
+       external_product_information: [ {
+    entity: [
+      { value: product.gtin || "N/A", type: product.gtinType || "EAN" }
+    ],language_tag: lang,
+    marketplace_id: MARKETPLACE_ID
   }],
         product_description: [{ value: product.description, language_tag: lang, marketplace_id: mid }],
         bullet_point: (product.bulletPoints.length ? product.bulletPoints : [product.description]).map(bp => ({
